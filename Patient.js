558860718650c32a297a1922ddecb2ee3e8254c4
Soubhagya244 <102404861+Soubhagya244@users.noreply.github.com>
2022-08-09 18:27:18 +0530

    Update Patient.js

--- Patient.js
+++ Patient.js
@@ -1,6 +1,4 @@
-const rxnorm  = "http://www.nlm.nih.gov/research/umls/rxnorm";
-const client  = new FHIR.client("https://r3.smarthealthit.org");
-const getPath = client.getPath;
+const client = new FHIR.client("https://r3.smarthealthit.org");
 
 function display(data) {
     const output = document.getElementById("output");
@@ -9,14 +7,6 @@ function display(data) {
         JSON.stringify(data, null, 4);
 }
 
-function getMedicationName(medCodings = []) {
-    var coding = medCodings.find(c => c.system === rxnorm);
-    return coding && coding.display || "Unnamed Medication(TM)";
-}
-
-client.request(`/MedicationRequest?patient=smart-1642068`, {
-    resolveReferences: "medicationReference"
-}).then(data => data.entry.map(item => getMedicationName(
-    getPath(item, "resource.medicationCodeableConcept.coding") ||
-    getPath(item, "resource.medicationReference.code.coding")
-))).then(display, display);
+client.request("/Encounter/3d1ae6a9-b8c1-4c12-a8ba-b44d96ddfe24", {
+    resolveReferences: ["serviceProvider", "subject"]
+}).then(display).catch(display);
