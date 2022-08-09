4991c421cfa3c59864c2df8028f1cbb4182f8b6e
Soubhagya244 <102404861+Soubhagya244@users.noreply.github.com>
2022-08-09 16:36:12 +0530

    Update Patient.js

--- Patient.js
+++ Patient.js
@@ -1,2 +1,11 @@
-const client = FHIR.client("https://r3.smarthealthit.org");
-client.request("Patient").then(console.log).catch(console.error);
+function display(data) {
+    const output = document.getElementById("output");
+    output.innerText = data instanceof Error ?
+        String(data) :
+        JSON.stringify(data, null, 4);
+}
+        
+const client = new FHIR.client("https://r3.smarthealthit.org");
+client.request("Patient/2e27c71e-30c8-4ceb-8c1c-5641e066c0a4")
+    .then(display)
+    .catch(display);
